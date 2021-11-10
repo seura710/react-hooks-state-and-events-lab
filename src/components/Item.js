f7703ae6419150d24843d34c29d87681d3eaac1b
Harris Kim <seura710@gmail.com>
2021-11-10 14:19:51 -0500

    Completed assignment

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 function Item({ name, category }) {
+
+  const [cartStatus , setCartStatus] = useState(false)
+  const classNameHandler = cartStatus ? "in-cart" : ""
+  
+  function changeClassName() {
+    setCartStatus(!cartStatus)
+  }
   return (
-    <li className="">
+    <li className={classNameHandler}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick = {changeClassName}>Add to Cart</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+
+  const [ selectedCategory , setSelectedCategory] = useState("All")
+
+  
+  function changeCategoryFilter(e) {
+    setSelectedCategory(e.target.value)
+    // const filteredList = items.filter((item) => item.category === e.target.value)
+    // setSelectedCategory(filteredList)
+  }
+  console.log(selectedCategory)
+
+  const itemsToDisplay = items.filter(
+      (item) => selectedCategory === "All" || item.category === selectedCategory
+    )
+
+  // You should also use the selectedCategory variable to determine which items to display in the shopping list. You'll need some way of filtering the array of items based on the selectedCategory.
+
   return (
     <div className="ShoppingList">
-      <div className="Filter">
-        <select name="filter">
+      <div className="Filter" onChange={changeCategoryFilter}>
+        <select name="filter" >
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +30,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+      {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
@@ -22,3 +39,5 @@ function ShoppingList({ items }) {
 }
 
 export default ShoppingList;
+
+
